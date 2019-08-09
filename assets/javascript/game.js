4a0a8f9183d0c8589b3393030f81edd85754c173
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-08-09 16:48:40 -0700

    pseudocoded sections and added global variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//GLOBAL VARIABLES
+//======================================================================
+//Arrays and Variables for holding data
+var wordOptions = ["sophia " + "petrillo", "rose " + "nylan", "blanche " + "devereaux", "dorothy " + "zbornak", "cheesecake", "miami"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+//Game counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 12;
+
+
+
+//FUNCTIONS
+//======================================================================
+
+//MAIN PROCESS
+//======================================================================
