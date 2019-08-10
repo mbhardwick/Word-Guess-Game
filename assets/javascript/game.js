7785d775d8eb87bb0663e0dfa82776f595c67ac7
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-08-09 18:28:59 -0700

    added functions for game start and linked to html display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 //GLOBAL VARIABLES
 //======================================================================
 //Arrays and Variables for holding data
-var wordOptions = ["sophia " + "petrillo", "rose " + "nylan", "blanche " + "devereaux", "dorothy " + "zbornak", "cheesecake", "miami"];
+var wordOptions = ["sophia" + "petrillo", "rose " + "nylan", "blanche " + "devereaux", "dorothy " + "zbornak", "cheesecake", "miami", "southern " + "bell", "sicily"];
 var selectedWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
@@ -17,6 +17,49 @@ var guessesLeft = 12;
 
 //FUNCTIONS
 //======================================================================
+//Start
+//select random word
+function startGame () {
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+
+    //breakdown word into individual letters
+    lettersinWord = selectedWord.split("");
+
+    //blanks required for word ----auto?
+    numBlanks = lettersinWord.length;
+
+    //Reset
+    guessesLeft = 12;
+    wrongLetters = [];
+    blanksAndSuccesses = [];
+
+    //Populate blanks and successes with right number of blanks
+    for (var i=0; i<numBlanks; i++){
+        blanksAndSuccesses.push("_");
+    }
+
+    //Change HTML to reflect round conditions
+    document.getElementById("current").innerHTML = blanksAndSuccesses.join("  ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
+    
+    //Testing / Debugging
+    console.log(selectedWord);
+    console.log(lettersinWord);
+    console.log(numBlanks);
+    console.log(blanksAndSuccesses);
+}
 
 //MAIN PROCESS
 //======================================================================
+//Initiates the code the first time
+startGame();
+
+//Event keyclick
+document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+
+    //Testing / Debugging
+    console.log(letterGuessed);
+}
